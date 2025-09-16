4eb7bea67eb70542491ce7b80b8e948161da86d6
Daniel <danieljrh.apple@gmail.com>
2025-09-16 17:36:22 +0800

    update

--- python_websocket/git_operations.py
+++ python_websocket/git_operations.py
@@ -2,7 +2,7 @@ import subprocess
 
 def check_update():
     result = subprocess.run(
-        ['git', 'rev-list', '--count', 'HEAD..github-release/release'],
+        ['git', 'rev-list', '--count', 'HEAD..origin/release'],
         cwd='/home/rpi/dartsnut_rpi',
         check=True,
         stdout=subprocess.PIPE,
@@ -17,8 +17,8 @@ def check_update():
 def perform_update():
     try:
         subprocess.run(['git', 'reset', '--hard'], cwd='/home/rpi/dartsnut_rpi', check=True)
-        subprocess.run(['git', 'fetch', 'github-release'], cwd='/home/rpi/dartsnut_rpi', check=True)
-        subprocess.run(['git', 'reset', '--hard', 'github-release/release'], cwd='/home/rpi/dartsnut_rpi', check=True)
+        subprocess.run(['git', 'fetch', 'origin'], cwd='/home/rpi/dartsnut_rpi', check=True)
+        subprocess.run(['git', 'reset', '--hard', 'origin/release'], cwd='/home/rpi/dartsnut_rpi', check=True)
         return {"action": "perform_update", "message": "Update successful"}
     except subprocess.CalledProcessError as e:
         return {"action": "perform_update", "error": f"Update failed: {str(e)}"}
